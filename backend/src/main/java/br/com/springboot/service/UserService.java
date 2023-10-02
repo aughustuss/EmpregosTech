d14ff170a7741f3484632005860d668d5061f94c
binudo <diogomf358@gmail.com>
2023-10-01 21:19:46 -0300

    at

--- backend/src/main/java/br/com/springboot/service/UserService.java
+++ backend/src/main/java/br/com/springboot/service/UserService.java
@@ -15,15 +15,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    private BCryptPasswordEncoder passwordEncoder;
-
-    public UserService() {
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
-
-    public BCryptPasswordEncoder passwordEncoder() {
-        return passwordEncoder;
-    }
 
     public List<User> listarTodos() {
         return userRepository.findAll();
@@ -38,18 +29,11 @@ public class UserService {
             throw new Error("Usuário já existe");
         }
 
-        String password = user.getSenha();
-        String encodedPassword = passwordEncoder.encode(password);
-        user.setSenha(encodedPassword);
         return userRepository.save(user);
     }
 
     public User autenticar(String username, String senha){
         User user = userRepository.findByUsername(username);
-
-        if (user != null && passwordEncoder.matches(senha, user.getSenha())) {
-            return user;
-        }
 
         return null;
     }
